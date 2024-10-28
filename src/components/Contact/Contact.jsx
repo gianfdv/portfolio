8bfa4bff6eed71f18d982fcd6f45d0689c9e8366
GianfrancoDV <vd60387@ufide.ac.cr>
2024-10-28 16:37:06 -0600

    Primer Commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:gdarsie@hotmail.com">gdarsie@hotmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/gianfranco-darsie/">linkedin.com/in/gianfranco-darsie</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/gianfdv">github.com/gianfdv</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Gianfranco D'Arsie</h1>
+        <p className={styles.description}>
+          Currently seeking an opportunity in the Machile Learning
+          and Data Science industry.
+        </p>
+        <a href="mailto:gdarsie@hotmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
